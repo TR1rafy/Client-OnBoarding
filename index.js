79dfc0d96d9ea48526e6ad95d55fd841353953a5
TR1rafy <tawfikrezarafy@gmail.com>
2022-12-10 19:48:50 +0600

    complete work

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ signUp.addEventListener("click", function(){
     const formArea = document.getElementById("form");
     formArea.style.display= "block";
 })
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyI37yHRXYtskZsgrhSqV5IY7HXVEVb6Ma40jWOvwUzHDN9m_ulgERE3GuXwL0JyEfycA/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg=document.getElementById("msg")
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response =>{
+      msg.innerHTML="Message Sent Succesfully"
+      setTimeout(function(){
+          msg.innerHTML=""
+      },5000)
+      form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
